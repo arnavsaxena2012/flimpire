f7a5531d4200f5898ebc9d628b8f2da1640749d8
Arnav Saxena <arnavsaxena@Arnavs-MacBook-Air.local>
2024-02-03 02:08:15 -0500

    Setup the components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
+import { Actors, Movies, MovieInformation, NavBar, Profile } from './index';
 
 const App = () => (
   <div>
-    App
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Switch>
+        <Route exact path="/movies/:id">
+          <MovieInformation />
+        </Route>
+        <Route exact path="/actors/:id">
+          <Actors />
+        </Route>
+        <Route exact path="/">
+          <Movies />
+        </Route>
+        <Route exact path="/profile/:id">
+          <Profile />
+        </Route>
+      </Switch>
+    </main>
   </div>
 );
 
